737c1339e71dd00516e327deda007c3ea27ccc43
yamaotakuya <takuya071021@gmail.com>
2026-06-01 07:01:46 +0900

    use environment variable for database url

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres.uwdlqlkjbesystnnwqlg:Takuya%40071021@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
